3579981acf1a36b76a0ee00fbc85efaac4b3c43c
Brian Genisio <BrianGenisio@Gmail.com>
2017-06-06 08:35:38 -0400

    convert to a query component

--- src/Schools.js
+++ src/Schools.js
@@ -1,77 +1,42 @@
 // @flow
 
 import React, {Component} from "react";
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 
-type School = {
-    id: string,
-    postalCode: string,
-    display: string,
-}
+import SchoolsQuery from "./SchoolsQuery.js";
 
 class Schools extends Component {
     props: {
         postalCode: string,
-
-        schools: Array<School>,
-        isLoading: boolean,
-        isEmpty: boolean,
-        isError: boolean,
     }
 
     render() {
-        const {schools, isLoading, isEmpty, isError} = this.props;
+        const {postalCode} = this.props;
 
-        if (isLoading) {
-            return <div>Loading</div>;
-        }
+        return <SchoolsQuery postalCode={postalCode}>
+            {(schools, {isLoading, isEmpty, isError}) => {
+                if (isLoading) {
+                    return <div>Loading</div>;
+                }
 
-        if (isEmpty) {
-            return <div>No items match your postal code</div>;
-        }
+                if (isEmpty) {
+                    return <div>No items match your postal code</div>;
+                }
 
-        if (isError) {
-            return <div>There was an error processing your search</div>;
-        }
+                if (isError) {
+                    return <div>There was an error processing your search</div>;
+                }
 
-        return <ul>{
-            schools.map((school) =>
-                <li key={school.id}>{school.display}</li>
-            )
-        }</ul>;
+                return <ul>{
+                    schools.map((school) =>
+                        <li key={school.id}>{school.display}</li>
+                    )
+                }</ul>;
+            }}
+        </SchoolsQuery>;
+        
+        
+         
     }
 }
 
-const SCHOOLS_QUERY = gql`query allSchools($postalCode: String!) {
-  allSchools(
-      orderBy: display_ASC,
-      filter: {postalCode: $postalCode}
-  ) {
-    id
-    postalCode
-    display
-  }
-}`;
-
-type SchoolsData =  {
-    loading: boolean,
-    error: string,
-    allSchools?: Array<School>,
-};
-
-const mapDataToProps = ({data}: {data: SchoolsData}) => {
-    const isLoading = data.loading;
-    const isEmpty = !!data.allSchools && data.allSchools.length === 0;
-    const isError = !!data.error;
-    const schools = data.allSchools || [];
-
-    return {isLoading, isEmpty, isError, schools};
-};
-
-const mapPropsToOptions = ({postalCode}) => ({variables: {postalCode}});
-
-export default graphql(SCHOOLS_QUERY, {
-    options: mapPropsToOptions,
-    props: mapDataToProps,
-})(Schools);
+export default Schools;
---
+++ src/SchoolsQuery.js
@@ -0,0 +1,77 @@
+// @flow
+import gql from 'graphql-tag';
+import {Component} from 'react';
+import {graphql} from 'react-apollo';
+
+type School = {
+    id: string,
+    postalCode: string,
+    display: string,
+}
+
+type SchoolQueryChildrenFn = (
+    schools: Array<School>,
+    details: {
+        isLoading: boolean,
+        isEmpty: boolean,
+        isError: boolean,
+    },
+) => React$Element<any>;
+
+class SchoolsQuery extends Component {
+    props: {
+        postalCode: string,
+        children?: SchoolQueryChildrenFn,
+
+        isLoading: boolean,
+        isEmpty: boolean,
+        isError: boolean,
+        schools: Array<School>,
+    }
+
+    static defaultProps = {
+        isLoading: false,
+        isEmpty: false,
+        isError: false,
+        schools: [],
+    }
+
+    render() {
+        const {isLoading, isEmpty, isError, schools, children} = this.props;
+
+        return children && children(schools, {isLoading, isEmpty, isError});
+    }
+}
+
+const SCHOOLS_QUERY = gql`query allSchools($postalCode: String!) {
+  allSchools(
+      orderBy: display_ASC,
+      filter: {postalCode: $postalCode}
+  ) {
+    id
+    postalCode
+    display
+  }
+}`;
+
+type SchoolsData =  {
+    loading: boolean,
+    error: string,
+    allSchools?: Array<School>,
+};
+
+const mapDataToProps = ({data}: {data: SchoolsData}) => {
+    const isLoading = data.loading;
+    const isEmpty = !!data.allSchools && data.allSchools.length === 0;
+    const isError = !!data.error;
+    const schools = data.allSchools || [];
+
+    return {isLoading, isEmpty, isError, schools};
+};
+
+const mapPropsToOptions = ({postalCode}) => ({variables: {postalCode}});
+
+export default graphql(SCHOOLS_QUERY, {
+    options: mapPropsToOptions,
+    props: mapDataToProps,
+})(SchoolsQuery);
