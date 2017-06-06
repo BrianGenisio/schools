3e0f140fbe8b4f6560e7a5db26853e7da97d324f
Brian Genisio <BrianGenisio@Gmail.com>
2017-06-05 21:20:19 -0400

    Get a simple Apollo example wired up
    Test Plan:
    none

---
+++ src/Schools.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class Schools extends Component {
+    render() {
+        if (this.props.data.loading) {
+            return (<div>Loading</div>)
+        }
+
+        return <ul>
+        {this.props.data.allSchools.map((school) =>
+            <li key={school.id}>{school.display}</li>
+        )}
+        </ul>;
+    }
+}
+
+const SCHOOLS_QUERY = gql`query allSchools {
+  allSchools(orderBy: display_ASC) {
+    id
+    postalCode
+    display
+  }
+}`;
+
+export default graphql(SCHOOLS_QUERY)(Schools);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj3kupprxfa090179mhocdhy7'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(<ApolloProvider client={client}>
+    <App />
+</ApolloProvider>, 
+document.getElementById('root'));
+
 registerServiceWorker();
